579c8cb3e44f32d290d19008e1c1b4ee419d090b
silivalleydev <silivalleydev@gmail.com>
2022-04-13 00:53:21 +0900

    webpack prod & dev config comment set

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -16,7 +16,7 @@ module.exports = (env, options) => {
     output: {
       filename: '[name].[hash].js',
       path: path.resolve(
-        __dirname + "/build"
+        __dirname + "/dev-build"
       ),
       publicPath: '/',
       pathinfo: false, // 포함된 모듈에 대한 정보를 주석으로 번들에 포함하도록 webpack에 지시 <-- default는 prod는 false, dev는 true
@@ -67,18 +67,48 @@ module.exports = (env, options) => {
       }),
     ],
     devServer: {
+      /**
+       * 핫 모듈 리플레이스 먼트 개념
+       * 웹팩 개발서버는 코드의 변화를 감지해서 전체 화면을 갱신하는데,
+       * SPA(싱글페이지 어플리케이션)는 브라우저에서 데이터를 들고 있기 때문에 
+       * 리프레시 이후에 입력한 모든 데이터가 초기화되는 불편한 상황이 생긴다.
+       * 
+       * 이러한 불편함을 해소하고자 생긴 기능이 바로 핫 모듈 리플레이스먼트이다.
+       * 핫 모듈 리플레이스먼트는 전체화면을 갱신하지 않고 코드가 변경된 모듈만 갱신 시켜주는 기능이다.
+       * 설정은 아래와 같이 devServer에 추가해주면된다.
+       * {
+       *    hot: true 
+       * }
+       */
+      hot: true,
+      // 서버가 로딩할 static 파일 경로 지정 
       contentBase: path.join(
         __dirname,
-        './build/'
+        './dev-build/'
       ),
       port: 3000,
-      historyApiFallback: true,
-      stats: {
-        color: true,
-      },
+      /**
+       * historyApiFallback은 HTML5의 History API를 사용하는 경우에, 
+       * 설정해놓은 url 이외의 url 경로로 접근했을때 404 responses를 받게 되는데 이때도 index.html을 서빙할지 결정하는 옵션이다.
+       * 
+       * "http://localhost:3000" 에 대해서만 server와 연결했다고 가정했을때
+       * 만약 리액트 라우팅으로 디테일 페이지로 들어갔고 그 라우팅을 detailpage라고 가정한다면
+       * 링크는 "http://localhost:3000/detailpage"가된다.
+       * 
+       * 하지만 http://localhost:3000/detailpage는 서버에 연결되지 않았기 때문에 새로고침을 하게되면
+       * 서버는 http://localhost:3000에 대해서만 연결했으니
+       * http://localhost:3000/detailpage로 연결요청을 하면 서버가 알아먹지 못하고 404 에러 페이지가 떠야한다
+       * 
+          React와 react-router-dom을 사용해 프로젝트를 만들때 react-router-dom이 내부적으로 HTML5 History API를 사용하므로 
+          미지정 경로로 이동했을때 혹은 그 상태에서 refresh를 했을때(새로고침)와 같은 경우에도 
+          애플리케이션이 적절히 렌더링을 할 수 있다. react 내부적으로 세션 기록을 저장 여부를 설정해주는 옵션이다. 
+
+          이로 인해 http://localhost:3080가 붙은 주소인 http://localhost:3000/detailpage도 마치 주소가 있는 것처럼
+          행동할 수 있게 해주는 옵션이다
+       */
+      historyApiFallback: true
     },
     resolve: {
-      // fallback: { "fs": false },      
       alias: {
         '@': path.resolve(__dirname, 'src/'),
         '@C': path.resolve(__dirname, 'src/components/'),
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -53,6 +53,25 @@ module.exports = (env, options) => {
           }
         })
       ] : [],  
+      /**
+       * 코드 스플리팅 개념
+       * 코드를 압축해도, 프로젝트가 커지면 브라우저 성능에 영향을 줄 수 있다.
+          결과물을 나누어 쪼개서 브라우저에서 다운로드 속도를 높인다.
+          큰 파일 하나를 다운받는 것 보다 작은 파일 여러개를 동시에 다운받는 것이 빠르다.
+
+          Optimization SplitChunks
+          이 문제를 해결하기 위해서 중복되는 요소를 하나의 Chunk(덩어리, 묶음)로 분리해서 사용하도록 해야한다. 
+          Webpack4부터 이 방식을 알아서 지원해주는 optimization.splitChunks.chunks를 이용하면, 
+          중복되는 Chunk 코드 요소를 줄여서 Bundle Size를 최적화 시킬 수 있다.
+
+          chunks option은 아래와 같다.
+
+          initial: 기본 Chunk Spliting.
+          async: 비동기로 호출하는 요소 중 중복되는 부분을 Spliting.
+          all: 모든 코드의 중복되는 요소들을 확인하여 Spliting.
+
+          Webpack 공식 Doc에는 'all'을 쓰는 것을 권장
+       */
       //전체 애플리케이션에서 node_modules의 모든 코드를 포함하는 vendors 청크를 만드는 설정
       splitChunks: {
         cacheGroups: {
@@ -67,8 +86,12 @@ module.exports = (env, options) => {
     module: {
       rules: [
         {
+          // test란 정규표현식으로 적용되는 파일들을 설정
           test: /\.(js|jsx)$/,
+          // exclude 컴파일하지 않을 폴더 설정
           exclude: '/node_modules',
+          // use에 사용할 로더 설정
+          // babel-loader는 최신 자바스크립트 문법(ES6, ES2020, ES2019 등등)을 commonJS로 변환 시켜줄수 있도록 도와주는 로더
           use: ['babel-loader'],
         },
         {
@@ -82,26 +105,90 @@ module.exports = (env, options) => {
         },
         {
           test: /\.css$/i,
-          use: ["style-loader", "css-loader"],
+          use: [
+            "style-loader", // CSS를 DOM에 삽입하는 로더
+            "css-loader" // import로 불러온 css파일을 해석하는 로더
+          ],
+        },
+        {
+          // file-loader는 import/ require()로 가져온 파일들을 output으로 생성하는것.
+          test: /\.(png|jpe?g|gif|mp3|mp4|wav|wma|ogg|ttf|woff2|woff|eot|svg|ico)$/i,
+          use: [
+              {
+                  loader: 'file-loader',
+                  options: {
+                      // 전체 디렉토리 구조를 유지하며 파일을 내보내려면 다음처럼 하면된다.
+                      name: '[path][name].[ext]'
+                  },
+
+              },
+          ],
         }
       ],
     },
     plugins: [
+      /**
+       * CleanWebpackPlugin은 빌드 이전 결과물을 제거하는 플러그인으로 
+       * 빌드 결과물은 웹팩에서 아웃풋 경로에 설장한 곳으로 폴더 및 파일들이 모이는데 
+       * 빌드 했을시 이전 빌드내용이 삭제되지 않고 그대로 남아있는 경우도 있어 이것을 해결해주는 플러그인이다.
+       */
       new CleanWebpackPlugin(),
+      /**
+       * HtmlWebPackPlugin
+       * html 파일을 템플릿으로 생성할 수 있게 도와주는 플러그인이다
+       */
       new HtmlWebPackPlugin({
         template: './index.html',
         filename: 'index.html',
       }),
+      /**
+       * MiniCssExtractPlugin
+       * 번들 결과에서 CSS를 별도의 파일로 추출하는 플러그인으로, 
+       * JS파일 당 CSS파일을 작성한다. 
+       * 브라우저에서 큰 파일을 하나 내려 받는 것보단 작은 파일을 여러개 다운로드하는 것이 더 빠르다.
+       */
+      new MiniCssExtractPlugin({ filename: '[name].css' }),
+      /**
+       * manifest.json을 생성해 매번 생성된 최종결과물과 매치되는 스태틱한 파일명을 
+       * alias와 같은 형태로 사용할 수 있게 해주므로 import시는 고정값을 사용가능하게 됨
+       * 
+       * 
+       * webpack resolve의 alias에 '@C': path.resolve(__dirname, 'src/components/') 라고 설정했다고 가정했을때,
+       * import CustomComponent from '@C/CustomComponent'; <= 이런식으로 import 가능하게 해줌
+       * 공통적으로 쓰이는 컴포넌트 경로를 import 할때 경로를 단축하여 코드를 작성할 수 있어 유용하다
+       */
       new ManifestPlugin({
         fileName: 'assets.json',
       }),
       // new BundleAnalyzerPlugin(),
+      /**
+       * DefinePlugin
+       *  컴파일 타임에 구성할 수 있는 전역 상수를 만들 수 있다. 
+       * 이는 개발 빌드와 프로덕션 빌드 간에 서로 다른 동작을 하고 싶을 때 유용하다.
+       * 
+       */
       new webpack.DefinePlugin({
         DEF_TARGET: JSON.stringify(options.target || 'web'),
         DEF_MODE: JSON.stringify(options.mode),
         APP_VERSION: JSON.stringify(version),
       }),
+      new CopyPlugin([{
+        // 어디에있는것을 복사하여 가져올지
+        from: './node_modules/axios/dist/axios.min.js',
+        to: './axios.min.js'
+      }])
     ],
+
+    /**
+     * 번들하지말아야할 대상은 빌드 범위에서 빼는 것
+      패키지들은 보통 빌드된 파일이 있다.
+      dist 폴더
+      copy-webpack-plugin: 웹팩이 실행될 때 패키지 빌드 파일을 복사
+     */
+    externals: {
+      // 웹팩으로 빌드시 axios 사용하는 부분이 있으면 전역변수 axios 사용하는 것으로 간주
+      axios: 'axios'
+    },
     resolve: {
       alias: {
         '@': path.resolve(__dirname, 'src/'),
